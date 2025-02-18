f0de5608b0818a2b5c71f749853579b248776437
AnjanaAnirudhan <23mp2139@rit.ac.in>
2025-02-18 20:08:04 +0530

    db config

--- dbConfig.js
+++ dbConfig.js
@@ -3,7 +3,7 @@ require('dotenv').config(); // Load environment variables
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongooose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
